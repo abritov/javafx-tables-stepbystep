feac546f5aca8f4915456061777efd51756f6f9c
envy124 <pkgsail.dev@gmail.com>
2022-07-20 20:46:30 +0300

    add lesson2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import javafx.application.Application;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Application.launch(FxApp.class, args);
     }
 }
---
+++ src/main/java/org/example/FxApp.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FxApp extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("JavaFX tables");
+        stage.show();
+    }
+}
