21036fe1e85e7096b52ea4083f4962e6d6e6e501
envy124 <pkgsail.dev@gmail.com>
2022-07-25 16:38:52 +0300

    add User model

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,19 @@
+package org.example.model;
+
+public class User {
+    private final Integer userId;
+    private final String name;
+
+    public User(Integer userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
